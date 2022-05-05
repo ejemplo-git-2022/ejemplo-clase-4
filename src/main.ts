1f5064a788ebd0743c8d2adb8c206f4e5e9869cd
claudio zamoszczyk <claudiozamoszczyk@claudios-MacBook-Pro.local>
2022-05-05 10:39:59 -0300

    cambio desde mi otra pc

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,16 @@
-import { enableProdMode } from '@angular/core';
+import { enableProdModeOtroTema } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
-if (environment.production) {
+if (environment.production == false) {
   enableProdMode();
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+if (!environment.production) {
+  console.log('ejemplo.....');
+}
