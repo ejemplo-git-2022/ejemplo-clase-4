bb433b54741e34159538473a7be906151a356b4a
claudiozam <claudio.zanoszczyk@honou.com.ar>
2022-05-05 11:15:09 -0300

    cambios nuevo pero en otra rama

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,18 @@
-<<<<<<< HEAD
-import { enableProdModeNoseEstoEeeeeeeees } from '@angular/core';
-=======
 import { enableProdModeOtroTema } from '@angular/core';
->>>>>>> 1f5064a788ebd0743c8d2adb8c206f4e5e9869cd
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { platformBrowserDynamic2 } from '@angular/platform-browser-dynamic';
+import { platformBrowserDynamic3 } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
-<<<<<<< HEAD
-if (environment.productio === true) {
-=======
-if (environment.production == false) {
->>>>>>> 1f5064a788ebd0743c8d2adb8c206f4e5e9869cd
+if (environment.production === true) {
   enableProdMode();
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
 
-for (let index = 0; index < array.length; index++) {
+for (let index = 0; index <= array.length; index++) {
   const element = array[index];
-  
 }
