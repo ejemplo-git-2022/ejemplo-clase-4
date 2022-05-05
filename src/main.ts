519254748ed7f7422060636d6b2a845c29bfd2f1
claudio zamoszczyk <claudiozamoszczyk@claudios-MacBook-Pro.local>
2022-05-05 11:16:22 -0300

    cambios en mi pc local

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,19 @@
 import { enableProdModeOtroTema } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { platformBrowserDynami99 } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
-if (environment.production == false) {
+if (environment.production === false) {
   enableProdMode();
+} else {
+
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
 
-if (!environment.production) {
+if (environment.production == true) {
   console.log('ejemplo.....');
 }
